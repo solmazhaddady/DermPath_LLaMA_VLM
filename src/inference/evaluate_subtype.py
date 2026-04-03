c53cc75f6eb4498133cd8fd0b89d3c3ffc302090
solmaz haddady <39735560+solmazhaddady@users.noreply.github.com>
2026-04-03 16:51:40 +0200

    Update evaluate_subtype.py

--- src/inference/evaluate_subtype.py
+++ src/inference/evaluate_subtype.py
@@ -16,7 +16,7 @@ import torch.nn as nn
 from pathlib import Path
 from sklearn.metrics import f1_score
 
-from training.train_subtype import SubtypeDataset
+from training.subtype_dataset import  SlideCriticalMultiLabel
 from training.train_subtype import SubtypeClassifier
 
 
